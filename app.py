74d0bdfab688291c6e6a8e22efb6353b6ee10408
Dinojan Jeewaraju <dinojanjeewaraju@gmail.com>
2026-04-06 09:22:42 +0000

    commit changes

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ num_of_students = len(student_books_list)
 print(f"There are {num_of_students} students in the class.")
 
 # sum / num of students
+
+#print(student_books_list[20])
+total_books = 0
+for invidual_books in student_books_list:
+  total_books += invidual_books
+print(f"total books {total_books}")
+
+print(f"The averae number of books read per student is {total_books/ num_of_students :.2f}")
