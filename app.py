bd8963857964d06b2e67562d1dc36a8cafa147ad
Dinojan Jeewaraju <dinojanjeewaraju@gmail.com>
2026-04-06 09:13:32 +0000

    commit changes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 
 # Problem Statement: Compute the average number of books each student has read this year
 
+students_book_list = [2,4,5,3,2,4,5,6,7,8,8,9,9,0,9,8]
+print(f"There are {len(students_book_list)} students in the class")
